93158cb4f9a9b17324799dccc11c36aa89a245d6
Akil <akilf@indeed.com>
2023-10-29 17:56:02 -0500

    Cannot get suggestion working

--- src/features/suggestion/suggestion.slice.js
+++ src/features/suggestion/suggestion.slice.js
@@ -3,8 +3,8 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 export const fetchSuggestion = createAsyncThunk(
   /* Task 15: Complete the `createAsyncThunk()` function to load a suggestion from 
     this URL: http://localhost:3004/api/suggestion */
-  "suggestion/getSuggestion",
-  async (localApi) => {
+  "suggestion/fetchSuggestion",
+  async (thunkAPI) => {
     const result = await fetch("http://localhost:3004/api/suggestion").then(
       (data) => data.json()
     );
@@ -24,10 +24,13 @@ const options = {
   reducers: {},
   extraReducers: {
     /* Task 16: Inside `extraReducers`, add reducers to handle all three promise lifecycle states - pending, fulfilled, and rejected - for the `fetchSuggestion()` call */
-    [fetchSuggestion.pending]: (state) => (state.loading = true),
+    [fetchSuggestion.pending]: (state) => {
+      state.loading = true;
+    },
     [fetchSuggestion.fulfilled]: (state, { payload }) => {
       state.loading = false;
       state.entities = payload;
+      state.error = false;
     },
     [fetchSuggestion.rejected]: (state) => {
       state.loading = false;
@@ -39,7 +42,8 @@ const suggestionSlice = createSlice(options);
 
 export default suggestionSlice.reducer;
 
-// Task 17: Create a selector, called `selectSuggestion`, for the `suggestion` state variable and export it from the file
+// Task 17: Create a selector, called `selectSuggestion`, for 
+// the `suggestion` state variable and export it from the file
 
 export const selectLoading = (state) => state.suggestion.loading;
 export const selectError = (state) => state.suggestion.error;
