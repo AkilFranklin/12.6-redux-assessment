93f2f7ceca65a3e56d05faede96cb24fd82e8549
Akil <akilf@indeed.com>
2023-10-29 17:24:53 -0500

    thunk not working, no course provided

--- src/features/suggestion/suggestion.slice.js
+++ src/features/suggestion/suggestion.slice.js
@@ -1,20 +1,37 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-export const fetchSuggestion =
-  createAsyncThunk(/* Task 15: Complete the `createAsyncThunk()` function to load a suggestion from this URL: http://localhost:3004/api/suggestion */);
+export const fetchSuggestion = createAsyncThunk(
+  /* Task 15: Complete the `createAsyncThunk()` function to load a suggestion from 
+    this URL: http://localhost:3004/api/suggestion */
+  "suggestion/getSuggestion",
+  async (localApi) => {
+    const result = await fetch("http://localhost:3004/api/suggestion").then(
+      (data) => data.json()
+    );
+    return result;
+  }
+);
 
 const initialState = {
-  suggestion: '',
+  suggestion: "",
   loading: false,
   error: true,
 };
 
 const options = {
-  name: 'suggestion',
+  name: "suggestion",
   initialState,
   reducers: {},
   extraReducers: {
     /* Task 16: Inside `extraReducers`, add reducers to handle all three promise lifecycle states - pending, fulfilled, and rejected - for the `fetchSuggestion()` call */
+    [fetchSuggestion.pending]: (state) => (state.loading = true),
+    [fetchSuggestion.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.entities = payload;
+    },
+    [fetchSuggestion.rejected]: (state) => {
+      state.loading = false;
+    }
   },
 };
 
@@ -26,3 +43,4 @@ export default suggestionSlice.reducer;
 
 export const selectLoading = (state) => state.suggestion.loading;
 export const selectError = (state) => state.suggestion.error;
+export const selectSuggestion = (state) => state.suggestion;
